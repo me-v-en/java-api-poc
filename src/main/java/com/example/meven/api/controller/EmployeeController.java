e7a282ba951b3b9c064df76c997460b0ed20c2f7
Meven <meven.ameline@gmail.com>
2024-08-21 16:35:12 +0200

    Implementing controller

---
+++ src/main/java/com/example/meven/api/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package com.example.meven.api.controller;
+
+import com.example.meven.api.model.Employee;
+import com.example.meven.api.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/employee/{id}")
+    public Employee updateEmployee(@PathVariable final Long id,@RequestBody Employee employee) {
+             return employeeService.updateEmployee(employee);
+    }
+
+    @GetMapping("/employee/{id}")
+    public Optional<Employee> getEmployee(@PathVariable("id") final Long id) {
+        return employeeService.getEmployee(id);
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public void deleteEmployee(@PathVariable("id") final Long id) {
+        employeeService.deleteEmployee(id);
+    }
+
+    @GetMapping("/employees")
+    public Iterable<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
--- src/main/java/com/example/meven/api/service/EmployeeService.java
+++ src/main/java/com/example/meven/api/service/EmployeeService.java
@@ -27,8 +27,27 @@ public class EmployeeService {
         employeeRepository.deleteById(id);
     }
 
-    public void saveEmployee(final Employee employee){
-        employeeRepository.save(employee);
+    public Employee updateEmployee(final Employee employee){
+        Optional<Employee> e =this.getEmployee(employee.getId());
+        if(e.isPresent()){
+            Employee updatedEmployee = e.get();
+            if(employee.getFirstName() != null){
+                updatedEmployee.setFirstName(employee.getFirstName());
+            }
+            if(employee.getLastName() != null){
+                updatedEmployee.setLastName(employee.getLastName());
+            }
+            if(employee.getMail() != null){
+                updatedEmployee.setMail(employee.getMail());
+            }
+            if(employee.getPassword() != null){
+                updatedEmployee.setPassword(employee.getPassword());
+            }
+
+            employeeRepository.save(updatedEmployee);
+            return updatedEmployee;
+        }
+        return null;
     }
 
 }

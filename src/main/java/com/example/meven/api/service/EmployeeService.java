a635de81fc0ad369c5b7a0a94479c4600c34eb24
Meven <meven.ameline@gmail.com>
2024-08-21 15:01:23 +0200

    Implementing service

--- src/main/java/com/example/meven/api/service/EmployeeService.java
+++ src/main/java/com/example/meven/api/service/EmployeeService.java
@@ -1,4 +1,34 @@
 package com.example.meven.api.service;
 
+import com.example.meven.api.model.Employee;
+import com.example.meven.api.repository.EmployeeRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Data
+@Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Optional<Employee> getEmployee(final long id){
+        return employeeRepository.findById(id);
+    }
+
+    public Iterable<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    public void deleteEmployee(final long id){
+        employeeRepository.deleteById(id);
+    }
+
+    public void saveEmployee(final Employee employee){
+        employeeRepository.save(employee);
+    }
+
 }
